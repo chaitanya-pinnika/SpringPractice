7298f51d4f01cfa7664f5425e8951aa371f0979e
chaitanya-pinnika <Pinnika@10.0.0.239>
2019-06-26 17:31:38 -0400

    First Commit
    No changes made

---
+++ BookPrinter/src/main/java/com/chaitanya/BookPrinter/BookPrinterApplication.java
@@ -0,0 +1,13 @@
+package com.chaitanya.BookPrinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPrinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPrinterApplication.class, args);
+	}
+
+}
---
+++ BookPrinter/src/test/java/com/chaitanya/BookPrinter/BookPrinterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chaitanya.BookPrinter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookPrinterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
