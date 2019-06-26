fb1b7acdd8b9724963ca3a4b53ad2796fc1471b7
chaitanya-pinnika <Pinnika@10.0.0.239>
2019-06-26 14:51:52 -0400

    First Commit
    Base application

---
+++ BookPrint/src/main/java/com/chaitanya/BookPrint/BookPrintApplication.java
@@ -0,0 +1,13 @@
+package com.chaitanya.BookPrint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPrintApplication.class, args);
+	}
+
+}
