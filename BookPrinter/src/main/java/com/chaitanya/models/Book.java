0799ac2c6398c2ffa0039c4c8790a00b0ce03cc6
chaitanya-pinnika <chaitanya.pinnika@gmail.com>
2019-06-26 17:41:00 -0400

    Added Book POJO
    Added getters and setters for Book Model

--- BookPrinter/src/main/java/com/chaitanya/models/Book.java
+++ BookPrinter/src/main/java/com/chaitanya/models/Book.java
@@ -1,5 +1,25 @@
 package com.chaitanya.models;
 
 public class Book {
+	String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBid() {
+		return bid;
+	}
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+	int bid;
+	@Override
+	public String toString() {
+		return "Book [name=" + name + ", bid=" + bid + "]";
+	}
+	
+	
 
 }
