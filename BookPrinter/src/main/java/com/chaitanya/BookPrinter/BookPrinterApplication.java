c16877754a3df25c508efcbfa1593e718b961e8b
chaitanya-pinnika <Pinnika@10.0.0.239>
2019-06-26 17:55:53 -0400

    Changes added
    I added changes in Main file

--- BookPrinter/src/main/java/com/chaitanya/BookPrinter/BookPrinterApplication.java
+++ BookPrinter/src/main/java/com/chaitanya/BookPrinter/BookPrinterApplication.java
@@ -2,12 +2,19 @@ package com.chaitanya.BookPrinter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.chaitanya.models.Book;
 
 @SpringBootApplication
 public class BookPrinterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookPrinterApplication.class, args);
+		ConfigurableApplicationContext context=SpringApplication.run(BookPrinterApplication.class, args);
+		
+		Book book = (Book) context.getBean(Book.class);
+		System.out.println(book);
+		
 	}
 
 }
