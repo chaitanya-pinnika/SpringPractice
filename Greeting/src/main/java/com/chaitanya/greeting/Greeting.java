f4842f5b2b12f5ddcbd194d02d001c9d458490d2
chaitanya-pinnika <Pinnika@10.0.0.239>
2019-06-27 14:25:47 -0400

    Added jsp
    Added a jsp and a dependecy to spport jsp based on tomcat version
    searched mvnrepository with ttomcat jasper

--- Greeting/src/main/java/com/chaitanya/greeting/Greeting.java
+++ Greeting/src/main/java/com/chaitanya/greeting/Greeting.java
@@ -3,17 +3,16 @@ package com.chaitanya.greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class Greeting {
 	
 	
 	@RequestMapping("hello")
-	@ResponseBody()
 	public String sayHello()
 	{
-		return "Hello";
+		return "Hello.jsp";
 	}
 	
 	
