d1ee7874add31278f75e7543c257248c2252e7a0
chaitanya-pinnika <Pinnika@10.0.0.239>
2019-07-01 15:54:39 -0400

    First Commit
    This rest example contains  Greeting class shared

---
+++ RestGreeting/src/main/java/com/chaitanya/greeting/RestGreetingApplication.java
@@ -0,0 +1,13 @@
+package com.chaitanya.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestGreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestGreetingApplication.class, args);
+	}
+
+}
