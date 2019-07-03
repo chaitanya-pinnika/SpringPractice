36ac7146df1bbfdfc6576ed98ff80cb616649b79
chaitanya-pinnika <Pinnika@10.0.0.239>
2019-07-03 14:39:49 -0400

    Added Jsp
    Jsp page has been added and we can access it using/getQuote

---
+++ RestQuote/src/main/java/com/chaitanya/quote/Controllers/QuoteController.java
@@ -0,0 +1,31 @@
+package com.chaitanya.quote.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import com.chaitanya.quote.models.Quote;
+
+@Controller
+public class QuoteController {
+	
+	Quote quote;
+
+	@RequestMapping("/getQuote")
+	public String getQuote(Model model) {
+		model.addAttribute("quoteobj",quote.toString());
+		return "randomquote";
+		
+	}
+	
+	@ModelAttribute
+	public void grabQuote()
+	{
+		RestTemplate rest = new RestTemplate();
+		quote = rest.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+		
+	}
+	
+}
